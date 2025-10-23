826e75358b974454c01e347ba1198c434d5039e9
keyvershe <keyshaafitha2@gmail.com>
2025-10-23 15:43:29 +0700

    Add files via upload

---
+++ tugas-6.py
@@ -0,0 +1,7 @@
+kendaraan = ["namakendaraan", "jeniskendaraan", "cckendaraan","warnakendaraan", "rodakendaraan"]
+
+kendaraan.extend(["hargakendaraan", "tipekendaraan"])
+
+kendaraan.insert(2, "merkkendaraan")
+
+print(kendaraan)
